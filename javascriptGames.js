190174b7ed5335b34bbb9bb6e2bb1752a161c0b7
Mrinalini Sinha <ms6471@nyu.edu>
2017-04-09 15:50:15 -0700

    Card Coutning function

---
+++ javascriptGames.js
@@ -0,0 +1,49 @@
+// You will write a card counting function. It will receive a card parameter and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative. The current count and the player's decision ("Bet" or "Hold") should be separated by a single space.
+//
+// Example Output
+// "-3 Hold"
+// "5 Bet"
+//
+// Hint
+// Do NOT reset count to 0 when value is 7, 8, or 9.
+
+
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count += 0;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+       count -= 1;
+       break;
+  }
+
+  currentCount = count.toString();
+
+  if (count > 0) {
+    return currentCount + ' Bet';
+  } else if (count <= 0) {
+    return currentCount + " Hold";
+  }
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
