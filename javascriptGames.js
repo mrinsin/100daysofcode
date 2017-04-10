ff52e5f84049b7f4f88357602e82094f0a4d2f06
Mrinalini Sinha <ms6471@nyu.edu>
2017-04-09 18:26:22 -0700

    nested for loops for accessing multidimensional arrays

--- javascriptGames.js
+++ javascriptGames.js
@@ -1,17 +1,13 @@
 // You will write a card counting function. It will receive a card parameter and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative. The current count and the player's decision ("Bet" or "Hold") should be separated by a single space.
-//
+
 // Example Output
 // "-3 Hold"
 // "5 Bet"
-//
-// Hint
-// Do NOT reset count to 0 when value is 7, 8, or 9.
 
 
 var count = 0;
 
 function cc(card) {
-  // Only change code below this line
   switch (card) {
     case 2:
     case 3:
@@ -41,14 +37,10 @@ function cc(card) {
   } else if (count <= 0) {
     return currentCount + " Hold";
   }
-  // Only change code above this line
 }
 
-// Add/remove calls to test your function.
-// Note: Only the last will display
 cc(2); cc(3); cc(7); cc('K'); cc('A');
 
-//
 // As we have seen in earlier examples, objects can contain both nested objects and nested arrays. Similar to accessing nested objects, Array bracket notation can be chained to access nested arrays.
 
 // Setup
@@ -71,6 +63,7 @@ var myPlants = [
  }
 ];
 
-// Only change code below this line
 
-var secondTree = myPlants[1].list[1]; // Change this line
+var secondTree = myPlants[1].list[1];
+
+// If you have a multi-dimensional array, you can use the same logic as the prior waypoint to loop through both the array and any sub-arrays. Here is an example:
